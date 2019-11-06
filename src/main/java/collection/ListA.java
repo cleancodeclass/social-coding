71570f52746f07a63b760e5add92618fad16edce
nubang7 <42139382+nubang7@users.noreply.github.com>
2019-11-06 13:46:14 +0900

    Create ListA.java

---
+++ src/main/java/collection/ListA.java
@@ -0,0 +1,39 @@
+package collection;
+
+public class ListA {
+	private static final int _10 = 10;
+	
+	public Object[] elements = new Object[10];
+	public boolean readOnly;
+	public int size = 0;
+	
+	public void add(Object element) {
+		if (readOnly) {
+			return;
+		} else {
+			int newSize = size + 1;
+			
+			if ( newSize > elements.length) {
+				Object[] newElements = new Object[elements.length + _10];
+				for (int i = 0; i < size; i++) {
+					newElements[i] = elements[i];
+				}
+				elements = newElements;
+			}
+
+			addElements(element);
+		}
+	}
+
+	private void addElements(Object element) {
+		elements[size] = element;
+		size++;
+	}
+	
+	public boolean setReadOnly(boolean ro) {
+		
+		readOnly= ro;
+		
+		return readOnly;
+	}
+}
