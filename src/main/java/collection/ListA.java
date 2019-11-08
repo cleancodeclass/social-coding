7f1b67a26fa45e0dced9e38c14590de34395ab84
nubang7 <42139382+nubang7@users.noreply.github.com>
2019-11-08 15:18:48 +0900

    ListA 초기화
    밋업 준비를 위해 ListA 클래스 초기화

--- src/main/java/collection/ListA.java
+++ src/main/java/collection/ListA.java
@@ -1,43 +1,39 @@
 package collection;
 
-public class ListA {     
+public class ListA {
+	private static final int _10 = 10;
 
-  private static final int _100 = 100;   
-	
 	public Object[] elements = new Object[10];
 	public boolean readOnly;
 	public int size = 0;
-	
+
 	public void add(Object element) {
 		if (readOnly) {
 			return;
 		} else {
 			int newSize = size + 1;
-			      
+
 			if ( newSize > elements.length) {
 				Object[] newElements = new Object[elements.length + _10];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
 				}
 				elements = newElements;
-			}   
+			}
 
 			addElements(element);
 		}
 	}
 
-	private void addElement(Object element) {
+	private void addElements(Object element) {
 		elements[size] = element;
 		size++;
-		size++;
-		size++;
-		size++;
 	}
-	
+
 	public boolean setReadOnly(boolean ro) {
-		
+
 		readOnly= ro;
-		
+
 		return readOnly;
 	}
 }
