f08edf85c1a3861147f1b2777829854ed85c5807
nubang7 <42139382+nubang7@users.noreply.github.com>
2019-11-12 17:47:43 +0900

    Techtoinc 클래스 준비를 위해, ListA 클래스 초기화
    Techtoinc 클래스 준비를 위해, ListA 클래스 초기화

--- src/main/java/collection/ListA.java
+++ src/main/java/collection/ListA.java
@@ -1,48 +1,32 @@
 package collection;
 
 public class ListA {
-
-	private static final int STORE_SIZE_INCREMENT = 10;              
-
 	public Object[] elements = new Object[10];
-
 	public boolean readOnly;
 	public int size = 0;
- 
-	public void add(Object element) {               
 
-		if (readOnly) {
-			//
-		} else {
+	public void add(Object element) {
+		if (!readOnly) {
 			int newSize = size + 1;
+			
+			if ( newSize > elements.length) {
+				Object[] newElements = new Object[elements.length + 10];
+				for (int i = 0; i < size; i++) {
+					newElements[i] = elements[i];
+				}
 
-			if (isElmentStoreFull(newSize)) {
-				increaseElementStore();
+				elements = newElements;
 			}
 
-			addElmentAtEnd(element);
+			elements[size] = element;
+			size++;
 		}
 	}
-
-	private void addElmentAtEnd(Object element) {
-		elements[size] = element;
-		size++;
-	}
-
-	private void increaseElementStore() {
-		Object[] newElements = new Object[elements.length + STORE_SIZE_INCREMENT];
-		for (int i = 0; i < size; i++) {
-			newElements[i] = elements[i];
-		}
-
-		elements = newElements;
-	}
-
-	private boolean isElmentStoreFull(int newSize) {
-		return newSize > elements.length;
-	}
-
-	public void setReadOnly(boolean b) {
-
+	
+	public boolean setReadOnly(boolean ro) {
+		
+		readOnly= ro;
+		
+		return readOnly;
 	}
 }
