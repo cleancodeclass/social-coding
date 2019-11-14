a3b7c3330be5da66adcc68dafd19c7b31b88b3b3
GeonHun Kim <dnatuna123@gmail.com>
2019-11-14 16:29:12 +0900

    [dnatuna] Magic number 제거 #6
    14번째 줄의 Magic number를 제거하였습니다.
    STORE_SIZE_INCREMENT 대체.

--- src/main/java/collection/ListA.java
+++ src/main/java/collection/ListA.java
@@ -4,13 +4,14 @@ public class ListA {
 	public Object[] elements = new Object[10];
 	public boolean readOnly;
 	public int size = 0;
+	private static final int STORE_SIZE_INCREMENT = 10;
 
 	public void add(Object element) {
 		if (!readOnly) {
 			int newSize = size + 1;
 			
 			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
+				Object[] newElements = new Object[elements.length + STORE_SIZE_INCREMENT];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
 				}
